3192077864569d9d09167abb8e43c022d1753a1a
Muhammad Mouiz Ghouri <mouizghouri77@gmail.com>
2024-07-30 09:25:33 +0200

    db fix

--- models/database.js
+++ models/database.js
@@ -5,13 +5,13 @@ const dbPath = path.resolve(__dirname, 'database.sqlite');
 const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
-  db.run(`CREATE TABLE users (
+  db.run(`CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE,
     password TEXT
   )`);
 
-  db.run(`CREATE TABLE posts (
+  db.run(`CREATE TABLE IF NOT EXISTS posts (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT,
     content TEXT,
